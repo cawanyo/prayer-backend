a53004a93b81428eee978977f4e330ea0cd0ab6c
chris_code <chris_code@sdsia.laas.fr>
2025-06-26 14:27:59 +0200

    fix requirements.txt

--- prayerBackend/settings.py
+++ prayerBackend/settings.py
@@ -136,7 +136,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://140.93.98.218:3000"
 ]
-
+ALLOWED_HOSTS = ["*"]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
